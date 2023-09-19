879db61deb071d3fe6c6eefb0db21df8a664e04b
Christian Helle <christian.helle@outlook.com>
2023-09-19 11:03:21 +0200

    Add example evil source generator

---
+++ sourcegenerator/EvilSourceGenerator.cs
@@ -0,0 +1,20 @@
+// using System.Diagnostics;
+// using Microsoft.CodeAnalysis;
+
+// [Generator]
+// public class EvilSourceGenerator : ISourceGenerator
+// {
+//     public void Execute(GeneratorExecutionContext context)
+//     {
+//     }
+
+//     public void Initialize(GeneratorInitializationContext context)
+//     {
+//         var psi = new ProcessStartInfo("https://github.com")
+//         {
+//             UseShellExecute = true,
+//             Verb = "open"
+//         };
+//         Process.Start(psi);
+//     }
+// }
