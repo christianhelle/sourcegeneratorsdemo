472bb66683a0d6159cc10b269a377ddef8b88e29
Christian Helle <christian.helle@outlook.com>
2023-09-19 13:53:31 +0200

    Add AutoCtor example

---
+++ sourcegeneratordemo/AutoCtorExample.cs
@@ -0,0 +1,13 @@
+﻿using AutoCtor;
+
+namespace SourceGeneratorDemo;
+
+public interface IFooService {}
+public interface IBarService {}
+
+[AutoConstruct]
+public class AutoCtorExample
+{
+    private readonly IFooService fooService;
+    private readonly IBarService barService;
+}
