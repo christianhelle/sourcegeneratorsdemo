ab0f169a9aaeced9f1f8a21bc5a8bed3c5194a7b
Christian Helle <christian.helle@outlook.com>
2023-09-19 11:51:23 +0200

    Implement ContractsGenerator

---
+++ sourcegenerator/ContractsGenerator.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using Microsoft.CodeAnalysis;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+[Generator]
+public class ContractsGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        foreach (AdditionalText file in context.AdditionalFiles)
+        {
+            var document = OpenApiDocument.FromFileAsync(file.Path).GetAwaiter().GetResult();
+            var generator = new CSharpClientGenerator(document, new CSharpClientGeneratorSettings
+            {
+                GenerateClientClasses = false,
+                GenerateDtoTypes = true,
+                GenerateClientInterfaces = false,
+                GenerateExceptionClasses = false,
+                CSharpGeneratorSettings =
+                {
+                    Namespace = "Generated.Contracts"
+                }
+            });
+            var csharp = generator.GenerateFile();
+            var fileInfo = new FileInfo(file.Path);
+            context.AddSource(fileInfo.Name, csharp);
+        }
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
